164d01d5db241162ae7f5686c2442be188a4aa65
alex Ko <konishchuk.sasha@gmail.com>
2023-01-31 11:27:53 +0200

    add some styles

--- src/components/MyDialog.vue
+++ src/components/MyDialog.vue
@@ -2,12 +2,15 @@
   <div class="modal-window-wrapper" v-show="show">
     <div class="modal-window">
       <button class="close-modal-window" @click="closeDialog">&times;</button>
-      <h2 class="delete-task-text">
+      <h2
+        class="delete-task-text"
+        :class="{ delete_task_hidden: taskBody.length > 100 }"
+      >
         Are you sure you want to delete task "<span
           :class="{ hiddenText: taskBody.length > 100 }"
-          >{{ taskBody }}</span
-        >
-        "?
+          >{{ taskBody }}
+        </span>
+        " ?
       </h2>
       <button class="btn delete-task-yes" @click="$emit('removeTask')">
         Yes
@@ -43,6 +46,16 @@ export default {
 
 <style lang="scss" scoped>
 .modal-window {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  background-color: rgb(255, 255, 255, 0.5);
+  padding: 6rem;
+  border-radius: 5px;
+  box-shadow: 0 3rem 5rem rgba(0, 0, 0, 0.3);
+  z-index: 10;
   .close-modal-window {
     position: absolute;
     top: 1.2rem;
@@ -54,15 +67,27 @@ export default {
     background: none;
   }
   .delete-task-text {
-    .hiddenText {
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
+    margin-bottom: 15px;
+    &.delete_task_hidden {
+      height: 140px;
       overflow: hidden;
+      position: relative;
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: -3px;
+        width: 100%;
+        height: 40px;
+        background: linear-gradient(180deg, transparent, #ccc 90%);
+      }
     }
-    @media screen and (max-width: 1170px) {
-      font-size: 1em;
-    }
+    // .hiddenText {
+    //   display: -webkit-box;
+    //   -webkit-line-clamp: 2;
+    //   -webkit-box-orient: vertical;
+    //   overflow: hidden;
+    // }
   }
   .btn {
     font-size: 1.4rem;
@@ -76,16 +101,36 @@ export default {
       border-radius: 0.25rem;
     }
   }
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 50%;
-  background-color: rgb(255, 255, 255, 0.5);
-  padding: 6rem;
-  border-radius: 5px;
-  box-shadow: 0 3rem 5rem rgba(0, 0, 0, 0.3);
-  z-index: 10;
+
+  @media screen and (max-width: 1170px) {
+    font-size: 1em;
+    width: 50%;
+    padding: 3rem;
+    .close-modal-window {
+      top: 1rem;
+      right: 0.5rem;
+      font-size: 3rem;
+      line-height: 0.5;
+    }
+    .btn {
+      font-size: 1.2rem;
+    }
+  }
+  @media (max-width: 992px) {
+    width: 70%;
+    .delete-task-text.delete_task_hidden {
+      height: 120px;
+    }
+  }
+  @media (max-width: 768px) {
+    width: 80%;
+    .delete-task-text.delete_task_hidden {
+      height: 110px;
+    }
+  }
+  @media (max-width: 576px) {
+    width: 90%;
+  }
 }
 
 .overlay {
